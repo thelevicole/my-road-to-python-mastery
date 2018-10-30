1c9feb45492ec0cc6225b8403d3f2a3eb5a2c778
Levi Cole <31482349+thelevicole@users.noreply.github.com>
2018-10-30 16:32:49 +0000

    String parser has own class

--- static-site-generator/generate.py
+++ static-site-generator/generate.py
@@ -14,7 +14,7 @@
 		{% endif %}
 
 	loop:
-		{% for item in list %}
+		{% for item in list %} | {% for key, value in list %}
 		{% endfor %}
 
 	while:
@@ -35,48 +35,60 @@
 import re
 import datetime
 
+class Parser():
+
+	#
+	# Regex matches
+	#
+	r_print		= r"({{\s*(.+?)\s*}})"
+
+	# Conditional
+	r_if		= r"({%\s*if\s*(.+?)\s*%}(.*?){%\s*endif\s*%})"
+	r_elseif	= r"({%\s*elseif\s*(.+?)\s*%})"
+	r_else		= r"({%\s*else\s*%})"
+
+	# Loops
+	r_for		= r"({%\s*for\s*(.+?,.+?|.+?)\s*in\s*(.+?)\s*%}(.*?){%\s*endfor\s*%})"
+	r_while		= r"({%\s*while\s*(.+?)\s*%}(.*?){%\s*endwhile\s*%})"
+
+	# Templating
+	r_extends	= r"({%\s*extends\s*(.+?)\s*%})"
+	r_section	= r"({%\s*section\s*(.+?)\s*%}(.*?){%\s*endsection\s*%})"
+	r_yeild		= r"({%\s*yeild\s*(.+?)\s*%})"
+
+	def __init__(self, string):
+		self.string = string
+
+	def conditional(self):
+		return re.findall(self.r_if, self.string, re.S)
+
+	def print(self):
+		return re.findall(self.r_print, self.string, re.S)
+
+	def loops(self):
+		return re.findall('|'.join([self.r_for, self.r_while]), self.string, re.S)
+
 
 class Template():
 
 	#
 	# Default data value
 	#
-	data		= {}
+	data = []
 
-	#
-	# Regex matches
-	#
-	r_print		= r"{{\s*(.+?)\s*}}" # {{ var }}
-
-	r_if		= r"{%\s*if\s*(.+?)\s*%}" # {% if (condition) %}
-	r_elseif	= r"{%\s*elseif\s*(.+?)\s*%}" # {% elseif (condition) %}
-	r_else		= r"{%\s*else\s*%}" # {% else %}
-	r_endif		= r"{%\s*endif\s*%}" # {% endif %}
-
-	r_for_s		= r"{%\s*for\s*(.+?,.+?|.+?)\s*in\s*(.+?) \s*%}" # {% for (key, value | value) in (list) %}
-	r_for_e		= r"{%\s*endfor\s*%}" # {% endfor %}
-
-	r_while_s	= r"{%\s*while\s*(.+?)\s*%}" # {% while (condition) %}
-	r_while_e	= r"{%\s*endwhile\s*%}" # {% endwhile %}
-
-	r_extends	= r"{%\s*extends\s*(.+?)\s*%}" # {% extends (filepath) %}
-
-	r_section_s	= r"{%\s*section\s*(.+?)\s*%}" # {% section (name) %}
-	r_section_e	= r"{%\s*endsection\s*%}" # {% endsection %}
-
-	r_yeild		= r"{%\s*yeild\s*(.+?)\s*%}" # {% yield (section) %}
-
-	def __init__(self, contents, path = '/', templates = '/'):
+	def __init__(self, contents = '', path = '/', templates = '/'):
 		self.contents	= contents	# Content to be parsed
 		self.path		= path		# Base path for includes..etc
 		self.templates	= templates	# Base path for templates
+
+		self.parser		= Parser(contents)
 
 	# Callback function for `re.sub`
 	#
 	# @param	Match	matches	Object passed by calling function
 	# @return	String
 	def _replace(self, matches):
-		match = matches.group(1)
+		match = matches.group(2)
 
 		# If is a direct variable match
 		if hasattr(self.data, match):
@@ -99,12 +111,23 @@ class Template():
 	# @param	String	string	This will be parsed with `r_print` regex
 	# @return	String
 	def _print(self, string):
-		return re.sub(self.r_print, self._replace, string)
+		return re.sub(self.parser.r_print, self._replace, string)
+
+	# Replace handlebar printing
+	#
+	# @param	String	string	This will be parsed with `r_if`, `r_elseif`, `r_else` and `r_endif` regex
+	# @return	String
+	def _condition(self, string):
+		pass
 
 
 	def render(self, data = {}):
 		self.data = data
-		return self._print( self.contents )
+
+		self.contents = self._print( self.contents )
+		#self.contents = self._condition( self.contents )
+
+		return self.contents
 
 
 
@@ -112,6 +135,19 @@ compiled = Template('''
 
 	Hello {{ name }}!
 	Last logged in: {{ date }}
+
+	{% if true %}
+		In if
+	{% else %}
+		In else
+	{% endif %}
+
+	{% if name %}
+	{% endif %}
+
+	{% for x in list %}
+		<!-- Do something -->
+	{% endfor %}
 
 ''').render({ \
 	'name': 'John Doe', \
